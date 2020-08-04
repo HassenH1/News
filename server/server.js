8030cc3222993bbef5254702ee8e4eea14eacfdd
HassenH1 <hasansaid51@gmail.com>
2020-08-03 18:46:50 -0700

    fixed cors issue

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const app = express();
 const port = 5000;
 const NewsAPI = require("newsapi");
 const newsapi = new NewsAPI("e3ad3410d8a347d8b68935d9ee916902");
+const cors = require("cors");
+
+app.use(cors()); //to fix cors issue
+app.use(express.json()); //like body-parser
 
 app.get("/", (req, res) => {
   newsapi.v2
--- src/component/Home.js
+++ src/component/Home.js
@@ -2,6 +2,22 @@ import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
 function Home() {
+  const fetchCall = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/", {
+        method: "GET",
+      });
+      const result = await response.json();
+      console.log(result, "<------result?");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchCall();
+  }, []);
+
   return (
     <>
       <br />
