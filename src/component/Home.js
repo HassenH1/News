8f163adea5f5a5eda6995e9c8a1f1f10b440e3b6
HassenH1 <hasansaid51@gmail.com>
2020-08-03 23:00:04 -0700

    .

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,30 +5,41 @@ import Context from "./Context/Context";
 function Home() {
   const { headliners } = useContext(Context);
 
-  let article = headliners?.articles?.map((article, i) => {
-    return (
-      <Row key={i}>
-        <Col style={{ border: "1px solid orange" }} className="mx-2 my-2">
-          {article.title}
-          {/* <Image src={article.urlToImage} rounded /> */}
-        </Col>
-        <Col style={{ border: "1px solid blue" }} className="mx-2 my-2">
-          {article.title}
-          {/* <Image src={article.urlToImage} rounded /> */}
-        </Col>
-        <Col style={{ border: "1px solid green" }} className="mx-2 my-2">
-          {article.title}
-          {/* <Image src={article.urlToImage} rounded /> */}
-        </Col>
-      </Row>
-    );
-  });
+  const article = headliners?.articles
+    ?.map((article, i) => {
+      // return (
+      //   <Row key={i}>
+      //     <Col style={{ border: "1px solid orange" }} className="mx-2 my-2">
+      //       {article.title}
+      //     </Col>
+      //     <Col style={{ border: "1px solid blue" }} className="mx-2 my-2">
+      //       {article.title}
+      //     </Col>
+      //     <Col style={{ border: "1px solid green" }} className="mx-2 my-2">
+      //       {article.title}
+      //     </Col>
+      //   </Row>
+      // );
+      return i % 3 === 0 ? headliners.articles.slice(i, i + 3) : null;
+    })
+    .filter((x) => x !== null);
 
   return (
     <>
       {console.log(headliners)}
       <br />
-      <Container>{article}</Container>
+      <Container>
+        {/* {article} */}
+        {article?.map((art, i) => {
+          return (
+            <Row>
+              {art.map((item) => (
+                <Col style={{ border: "1px solid red" }}>{item.title}</Col>
+              ))}
+            </Row>
+          );
+        })}
+      </Container>
     </>
   );
 }
