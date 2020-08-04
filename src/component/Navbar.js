56e655ae98323727d1e7d745f75bbf9f4249d596
HassenH1 <hasansaid51@gmail.com>
2020-08-03 17:29:19 -0700

    navbar

---
+++ src/component/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Navbar, Nav, Button, Form, FormControl } from "react-bootstrap";
+
+function NavbarComponent() {
+  return (
+    <Navbar bg="light" variant="light">
+      <Navbar.Brand href="#testRoute">News</Navbar.Brand>
+      <Nav className="mr-auto"></Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-primary">Search</Button>
+      </Form>
+    </Navbar>
+  );
+}
+
+export default NavbarComponent;
