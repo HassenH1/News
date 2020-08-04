d98cd1348f22cb3e26cd9d8b94721d975cad2c98
HassenH1 <hasansaid51@gmail.com>
2020-08-03 18:11:33 -0700

    server setup

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const NewsAPI = require("newsapi");
+const newsapi = new NewsAPI("e3ad3410d8a347d8b68935d9ee916902");
+
+app.get("/", (req, res) => {
+  newsapi.v2
+    .topHeadlines({
+      language: "en",
+      country: "us",
+    })
+    .then((response) => {
+      console.log(response, "<=====the response is?");
+      /*
+      {
+        status: "ok",
+        articles: [...]
+      }
+    */
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <br />
+      <Container>
+        <Row>
+          <Col style={{ border: "1px solid orange" }}></Col>
+          <Col style={{ border: "1px solid blue" }}></Col>
+          <Col style={{ border: "1px solid green" }}></Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default Home;
